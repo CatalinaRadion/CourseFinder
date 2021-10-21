faa23cc161c3da03e05389b7436e8dd52160a8e4
Radu Onea <radu.onea@ibm.com>
2021-10-21 15:51:16 +0300

    Initial commit

---
+++ src/main/java/com/ibm/coursefinder/CourseFinderApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.coursefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseFinderApplication.class, args);
+    }
+
+}
