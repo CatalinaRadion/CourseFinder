8d292d16ddaad3e59ee4b509b0b6dd58b5edc5bb
ZZ032C826 <radu.onea@ibm.com>
2021-11-09 11:59:50 +0200

    custom headers and status

--- src/main/java/com/ibm/coursefinder/controllers/IndexController.java
+++ src/main/java/com/ibm/coursefinder/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.ibm.coursefinder.controllers;
 
 import com.ibm.coursefinder.seeders.InitialSeeder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,5 +17,11 @@ public class IndexController {
     @GetMapping("")
     public String index() {
         return "index";
+    }
+    @GetMapping("/brewCoffee")
+    public ResponseEntity<String> brewCoffee(){
+       return ResponseEntity.status(418)
+               .header("Teapot","I am not")
+               .body("I'm not gonna brew any coffee");
     }
 }
